d14f2c2d9741ba1beabbe0144c9a9ae246e5029d
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-05-18 20:10:08 +0200

    mopdifiche

--- machinedinlet.js
+++ machinedinlet.js
@@ -6,10 +6,10 @@ robot.setKeyboardDelay(200)
 robot.setMouseDelay(100)
 
 //open FC8
-robot.keyTap('command')
-robot.typeString('C:\\FlowConsultant8\\FC8.exe')
-robot.keyTap('enter')
-sleep.msleep(500)
+//robot.keyTap('command')
+//robot.typeString('C:\\FlowConsultant8\\FC8.exe')
+//robot.keyTap('enter')
+//sleep.msleep(500)
 
 //insert data
 robot.keyTap('tab')
