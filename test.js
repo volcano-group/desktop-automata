b009a078d7858d04a595e902dd39834aee2c6d18
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-05-20 16:59:54 +0200

    some doc

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 import test from './fc8_main'
+//fare riferimento a fc8_venturi_map per vedere gli input disponibili
+//compilare i json "User Defined" solo se nei relativi input è stato inserito "userdefined"
 test.machinedinlet("Upstream", 
                    "Duplex",
-                   {name: "", coeffThermalExpansion: ""} , 
+                   {id: "User Defined Pipe Material", name: "", coeffThermalExpansion: ""} , 
                    "userdefined",
-                   {name: "Carbon Steel", coeffThermalExpansion: "0.00000608"},
+                   {id:"User Defined Venturi Cylindrical Throat", name: "Carbon Steel", coeffThermalExpansion: "0.00000608"},
                    "McOn")
