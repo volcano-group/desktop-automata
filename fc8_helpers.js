b72e22cf3c6efd097a50d0c2fac0ad52a0edea26
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-06-14 18:30:49 +0200

    fix

--- fc8_helpers.js
+++ fc8_helpers.js
@@ -191,7 +191,7 @@ exports.printPDF = function(fileName, customer, tag){
 
 exports.existFile = function (fileName, errors) {
     console.log("controllo esistenza file ", fileName)
-    fs.existsSync("C:/Users/Administrator/Documents/"+fileName, function(exists) {
+    fs.exists('C:/Users/Administrator/Documents/'+fileName, function(exists) {
         console.log(exists)
         if(!exists){
             errors.push(fileName)
