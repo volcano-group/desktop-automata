50d022b938be23edfba06f4e094889984ee31fbf
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-06-15 18:42:26 +0200

    fix

--- test.js
+++ test.js
@@ -161,6 +161,58 @@ let request = {
             "heatingTemperature": "15.56",
             "isentropicExponent": "1.364"
           }
+        },
+        {
+          "pipe": {
+            "type": "Orifice",
+            "type2": "Flange Taps",
+            "style": "Clamped",
+            "standard": "ISO-5167 (2003-), ASME 3M (2004-)",
+            "pressuretap": "Downstream",
+            "pipematerial": "Carbon Steel",
+            "primaryelement": "316",
+            "special": {
+              "DrainHole": "on",
+              "holes": "1"
+            },
+            "gasUnits": "US gas"
+          },
+          "tag": "1113",
+          "calculation": {
+            "ventDrainHole": "0.1250",
+            "base": {
+              "pipeDiameter": "102.331",
+              "borePrimaryElement": "57.51",
+              "size": {
+                "maxFlow": "60000000000.0000",
+                "normalFlow": "4245.0000",
+                "differential": "2500"
+              }
+            },
+            "method": "size"
+          },
+          "gas": {
+            "dimensionalUnit": "Millimetre",
+            "pressure": "25",
+            "pressureUnit": "Bar,abs",
+            "differentialUnit": "mm of Water @ 4 degC",
+            "temperature": "25",
+            "temperatureUnit": "TdegC",
+            "flowingDensity": "20.71",
+            "flowMethod": "Mass",
+            "flowUnit": "Mass kg",
+            "densityMassUnit": "Kilogram (ISO Standards)",
+            "densityVolumeUnit": "Cubic Metre",
+            "densityMethod": "Density",
+            "viscosity": "0.012",
+            "viscosityUnit": "Centipoise cP",
+            "gasType": "Enter Fluid Properties",
+            "fluidProperties": {
+              "name": "Natural Gas"
+            },
+            "heatingTemperature": "15.56",
+            "isentropicExponent": "1.364"
+          }
         }
       ]
 }
