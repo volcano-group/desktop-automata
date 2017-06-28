b22d64d3d08b322573360162d9e569ad69a7faf9
Alessandro Locatelli <alessandro.locatelli95@gmail.com>
2017-06-28 19:08:42 +0200

    fix

--- test.js
+++ test.js
@@ -5,298 +5,56 @@ let request =  {
       "state": "new",
       "createdAt": 1498665082391,
       "customer": "Volcano Group",
-      "items": [
-        {
-          "tag": "120-FE-1904",
+      "items": [ {
+          "tag": "120-FE-0051",
           "pipe": {
-              "type": "Orifice",
             "type2": "Flange Taps",
             "style": "Clamped",
             "standard": "ISO-5167 (2003-), ASME 3M (2004-)",
             "pressuretap": "Downstream",
-            "pipematerial": "Carbon Steel",
+            "pipematerial": "304",
             "primaryelement": "316",
             "special": {
               "DrainHole": "on",
               "holes": "1"
             },
-            "gasUnits": "US Gas"
+            "gasUnits": "US Liquid"
           },
           "gas": {
             "dimensionalUnit": "Millimetre",
-            "pressure": "36",
+            "pressure": "38,1",
             "pressureUnit": "Bar,gage",
             "differentialUnit": "kPa",
-            "temperature": "40",
+            "temperature": "115",
             "temperatureUnit": "TdegC",
-            "flowingDensity": "15800",
-            "flowMethod": "Base",
-            "flowUnit": "Base Cubic Metres: for NCMH, etc.",
-            "basePressure": "1,013",
-            "basePressureUnit": "Bar,abs",
-            "baseTemperature": "0",
-            "baseTemperatureUnit": "TdegC",
+            "flowingDensity": "243044",
+            "flowMethod": "Flowing",
+            "flowUnit": "Flowing m^3",
             "densityMassUnit": "Kilogram (ISO Standards)",
             "densityVolumeUnit": "Cubic Metre",
-            "densityMethod": "pvT pf, Tf, Zf, Mw (Molecular Weight)",
-            "densitySpecial": {
-              "molecularWeight": "28,01",
-              "flowingCompressibilityFactor": "0,99"
-            },
-            "viscosity": "0.019",
+            "densityMethod": "Density",
+            "viscosity": "0.243",
             "viscosityUnit": "Centipoise cP",
             "gasType": "Enter Fluid Properties",
             "fluidProperties": {
-              "name": "High pressure nitrogen"
+              "name": "Stripped Process Condensate"
             },
-            "heatingTemperature": "15.56",
-            "isentropicExponent": "1,41"
+            "heatingTemperature": "15.56"
           },
           "calculation": {
             "base": {
-              "pipeDiameter": "154,08",
+              "pipeDiameter": "193,67",
               "size": {
-                "maxFlow": "17000",
-                "normalFlow": "15800",
-                "differential": "28"
+                "maxFlow": "363586,3",
+                "normalFlow": "243044",
+                "differential": "26"
               }
             },
             "method": "size",
             "ventDrainHole": "0.0938"
           }
         },
-          {
-          "tag": "120-FE-1904",
-          "pipe": {
-            "type2": "Flange Taps",
-            "style": "Clamped",
-            "standard": "ISO-5167 (2003-), ASME 3M (2004-)",
-            "pressuretap": "Downstream",
-            "pipematerial": "Carbon Steel",
-            "primaryelement": "316",
-            "special": {
-              "DrainHole": "on",
-              "holes": "1"
-            },
-            "gasUnits": "US Gas"
-          },
-          "gas": {
-            "dimensionalUnit": "Millimetre",
-            "pressure": "36",
-            "pressureUnit": "Bar,gage",
-            "differentialUnit": "kPa",
-            "temperature": "40",
-            "temperatureUnit": "TdegC",
-            "flowingDensity": "15800",
-            "flowMethod": "Base",
-            "flowUnit": "Base Cubic Metres: for NCMH, etc.",
-            "basePressure": "1,013",
-            "basePressureUnit": "Bar,abs",
-            "baseTemperature": "0",
-            "baseTemperatureUnit": "TdegC",
-            "densityMassUnit": "Kilogram (ISO Standards)",
-            "densityVolumeUnit": "Cubic Metre",
-            "densityMethod": "pvT pf, Tf, Zf, Mw (Molecular Weight)",
-            "densitySpecial": {
-              "molecularWeight": "28,01",
-              "flowingCompressibilityFactor": "0,99"
-            },
-            "viscosity": "0.019",
-            "viscosityUnit": "Centipoise cP",
-            "gasType": "Enter Fluid Properties",
-            "fluidProperties": {
-              "name": "High pressure nitrogen"
-            },
-            "heatingTemperature": "15.56",
-            "isentropicExponent": "1,41"
-          },
-          "calculation": {
-            "base": {
-              "pipeDiameter": "154,08",
-              "size": {
-                "maxFlow": "17000",
-                "normalFlow": "15800",
-                "differential": "28"
-              }
-            },
-            "method": "size",
-              "ventDrainHole": "0.0938"
-          }
-        },
-        {
-          "tag": "120-FE-1905",
-          "pipe": {
-            "type2": "Flange Taps",
-            "style": "Clamped",
-            "standard": "ISO-5167 (2003-), ASME 3M (2004-)",
-            "pressuretap": "Downstream",
-            "pipematerial": "Carbon Steel",
-            "primaryelement": "316",
-            "special": {
-              "DrainHole": "on",
-              "holes": "1"
-            },
-            "gasUnits": "US Gas"
-          },
-          "gas": {
-            "dimensionalUnit": "Millimetre",
-            "pressure": "4",
-            "pressureUnit": "Bar,gage",
-            "differentialUnit": "kPa",
-            "temperature": "40",
-            "temperatureUnit": "TdegC",
-            "flowingDensity": "240",
-            "flowMethod": "Base",
-            "flowUnit": "Base Cubic Metres: for NCMH, etc.",
-            "basePressure": "1,013",
-            "basePressureUnit": "Bar,abs",
-            "baseTemperature": "0",
-            "baseTemperatureUnit": "TdegC",
-            "densityMassUnit": "Kilogram (ISO Standards)",
-            "densityVolumeUnit": "Cubic Metre",
-            "densityMethod": "pvT pf, Tf, Zf, Mw (Molecular Weight)",
-            "densitySpecial": {
-              "molecularWeight": "28,01",
-              "flowingCompressibilityFactor": "0,99"
-            },
-            "viscosity": "0.019",
-            "viscosityUnit": "Centipoise cP",
-            "gasType": "Enter Fluid Properties",
-            "fluidProperties": {
-              "name": "Low pressure nitrogen"
-            },
-            "heatingTemperature": "15.56",
-            "isentropicExponent": "1,4"
-          },
-          "calculation": {
-            "base": {
-              "pipeDiameter": "154,08",
-              "size": {
-                "maxFlow": "8140",
-                "normalFlow": "240",
-                "differential": "29"
-              }
-            },
-            "method": "size",
-              "ventDrainHole": "0.0938"
-          }
-        },
-        {
-          "tag": "120-FE-2904",
-          "pipe": {
-            "type2": "Flange Taps",
-            "style": "Clamped",
-            "standard": "ISO-5167 (2003-), ASME 3M (2004-)",
-            "pressuretap": "Downstream",
-            "pipematerial": "Carbon Steel",
-            "primaryelement": "316",
-            "special": {
-              "DrainHole": "on",
-              "holes": "1"
-            },
-            "gasUnits": "US Gas"
-          },
-          "gas": {
-            "dimensionalUnit": "Millimetre",
-            "pressure": "36",
-            "pressureUnit": "Bar,gage",
-            "differentialUnit": "kPa",
-            "temperature": "40",
-            "temperatureUnit": "TdegC",
-            "flowingDensity": "15800",
-            "flowMethod": "Base",
-            "flowUnit": "Base Cubic Metres: for NCMH, etc.",
-            "basePressure": "1,013",
-            "basePressureUnit": "Bar,abs",
-            "baseTemperature": "0",
-            "baseTemperatureUnit": "TdegC",
-            "densityMassUnit": "Kilogram (ISO Standards)",
-            "densityVolumeUnit": "Cubic Metre",
-            "densityMethod": "pvT pf, Tf, Zf, Mw (Molecular Weight)",
-            "densitySpecial": {
-              "molecularWeight": "28,01",
-              "flowingCompressibilityFactor": "0,99"
-            },
-            "viscosity": "0.019",
-            "viscosityUnit": "Centipoise cP",
-            "gasType": "Enter Fluid Properties",
-            "fluidProperties": {
-              "name": "High pressure nitrogen"
-            },
-            "heatingTemperature": "15.56",
-            "isentropicExponent": "1,41"
-          },
-          "calculation": {
-            "base": {
-              "pipeDiameter": "154,08",
-              "size": {
-                "maxFlow": "17000",
-                "normalFlow": "15800",
-                "differential": "30"
-              }
-            },
-            "method": "size",
-              "ventDrainHole": "0.0938"
-          }
-        },
-        {
-          "tag": "120-FE-2905",
-          "pipe": {
-            "type2": "Flange Taps",
-            "style": "Clamped",
-            "standard": "ISO-5167 (2003-), ASME 3M (2004-)",
-            "pressuretap": "Downstream",
-            "pipematerial": "Carbon Steel",
-            "primaryelement": "316",
-            "special": {
-              "DrainHole": "on",
-              "holes": "1"
-            },
-            "gasUnits": "US Gas"
-          },
-          "gas": {
-            "dimensionalUnit": "Millimetre",
-            "pressure": "4",
-            "pressureUnit": "Bar,gage",
-            "differentialUnit": "kPa",
-            "temperature": "40",
-            "temperatureUnit": "TdegC",
-            "flowingDensity": "240",
-            "flowMethod": "Base",
-            "flowUnit": "Base Cubic Metres: for NCMH, etc.",
-            "basePressure": "1,013",
-            "basePressureUnit": "Bar,abs",
-            "baseTemperature": "0",
-            "baseTemperatureUnit": "TdegC",
-            "densityMassUnit": "Kilogram (ISO Standards)",
-            "densityVolumeUnit": "Cubic Metre",
-            "densityMethod": "pvT pf, Tf, Zf, Mw (Molecular Weight)",
-            "densitySpecial": {
-              "molecularWeight": "28,01",
-              "flowingCompressibilityFactor": "0,99"
-            },
-            "viscosity": "0.019",
-            "viscosityUnit": "Centipoise cP",
-            "gasType": "Enter Fluid Properties",
-            "fluidProperties": {
-              "name": "Low pressure nitrogen"
-            },
-            "heatingTemperature": "15.56",
-            "isentropicExponent": "1,4"
-          },
-          "calculation": {
-            "base": {
-              "pipeDiameter": "154,08",
-              "size": {
-                "maxFlow": "8140",
-                "normalFlow": "240",
-                "differential": "31"
-              }
-            },
-            "method": "size",
-              "ventDrainHole": "0.0938"
-          }
-        }
+        
       ]
     }
 
